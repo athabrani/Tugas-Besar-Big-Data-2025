c63199b45d5d7c1ba4c6985ad779dd68d0060380
Maulana Hafiz <manl.hzzz01@gmail.com>
2026-01-05 22:03:48 +0700

    Refactor import order and clean up code

--- etl_pipeline/extract_source2.py
+++ etl_pipeline/extract_source2.py
@@ -1,25 +1,33 @@
-import os, json, time
+import time, json
 from pathlib import Path
-
-import numpy as np
 import pandas as pd
-import re
 from pytrends.request import TrendReq
+from extract_source1 import append_jsonl
+
+BASE_DIR = Path("/content/bigdata_final_project")
+RAW_DIR = BASE_DIR / "raw"
+LOG_DIR = BASE_DIR / "etl_pipeline" / "logs"
+
+EXTRACT_LOG = LOG_DIR / "extract_log.jsonl"
+
+def derive_timeframe_from_sales(raw_sales_path: Path) -> str:
+    tmp = pd.read_csv(raw_sales_path)
+    tmp.columns = [c.strip().lower().replace(" ", "_") for c in tmp.columns]
+    dt = pd.to_datetime(tmp["transaction_date"], errors="coerce")
+    return f"{dt.min().date()} {dt.max().date()}"
 
 def extract_etl_source2(raw_sales_path: Path) -> Path:
     keywords = ["coffee", "bakery", "tea", "chocolate"]
     geo = "US-NY"
 
     timeframe = derive_timeframe_from_sales(raw_sales_path)
-
     out_path = RAW_DIR / "source2_pytrends_interest_over_time_raw.csv"
 
     t0 = time.time()
     pytrends = TrendReq(hl="en-US", tz=420)
     pytrends.build_payload(keywords, timeframe=timeframe, geo=geo)
-    iot = pytrends.interest_over_time()   # RAW
-
-    iot.to_csv(out_path, index=True)      # RAW save (index date)
+    iot = pytrends.interest_over_time()
+    iot.to_csv(out_path, index=True)
     exec_s = time.time() - t0
 
     append_jsonl(EXTRACT_LOG, {
